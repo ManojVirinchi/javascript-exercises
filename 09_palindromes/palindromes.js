0aabfd09e3066933b1b2b193c23fe6587b692399
ManojVirinchi <virinchimanoj@gmail.com>
2025-01-22 15:30:19 -0500

    updated

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const alphanumer = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    const temp = str.toLowerCase().split('').filter((character)=>{return alphanumer.includes(character);})
+    .join('');
+
+    const temp2 = temp.split('').reverse().join('');
+
+    return temp===temp2;
 };
 
 // Do not edit below this line
